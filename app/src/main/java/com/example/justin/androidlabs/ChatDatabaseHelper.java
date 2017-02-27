41e96fe9f72306cb45b48385702b4c5041ea23b7
Justin <rich0410@algonquinlive.com>
2017-02-27 12:37:50 -0500

    Lab5 Finished

---
+++ app/src/main/java/com/example/justin/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,41 @@
+package com.example.justin.androidlabs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Justin on 2017-02-27.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+    protected static String DATABASE_NAME = "MessageTable", KEY_ID = "MessageNumber", KEY_MESSAGE = "Message";
+    protected static int VERSION_NUM = 1;
+
+    public ChatDatabaseHelper(Context ctx){
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+        db.execSQL("CREATE TABLE " + DATABASE_NAME + " ("+KEY_ID+" STRING PRIMARY KEY AUTO-INCREMENT, "+KEY_MESSAGE+" String);" );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+        db.execSQL("DROP TABLE IF EXIST "+DATABASE_NAME);
+        onCreate(db);
+    }
+
+    public String getKeyMessage(){
+        return KEY_MESSAGE;
+    }
+
+    public String getDATABASE_NAME(){
+        return DATABASE_NAME;
+    }
+
+}
--- app/src/main/java/com/example/justin/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/justin/androidlabs/ChatWindow.java
@@ -1,8 +1,12 @@
 package com.example.justin.androidlabs;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +25,11 @@ public class ChatWindow extends AppCompatActivity {
     ListView lView;
     ArrayList<String> chatList;
     ChatAdapter messageAdapter;
+    ChatDatabaseHelper cDBH;
+    SQLiteDatabase dbWrite;
+    Cursor c;
+    ContentValues cv;
+    protected static final String activity = "Chat Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +40,38 @@ public class ChatWindow extends AppCompatActivity {
         lView = (ListView) findViewById(R.id.listView);
         chatList = new ArrayList<String>();
         messageAdapter =new ChatAdapter( this );
+        cDBH = new ChatDatabaseHelper(this);
+        dbWrite= cDBH.getWritableDatabase();
+        c = dbWrite.rawQuery("SELECT Message FROM ?", new String[]{"MessageTable"});
+        cv = new ContentValues();
+
         lView.setAdapter (messageAdapter);
+        textBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textBox.setText("");
+            }
+        });
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 chatList.add(textBox.getText().toString());
+                cv.put(cDBH.getKeyMessage(), textBox.getText().toString());
+                dbWrite.insert(cDBH.getDATABASE_NAME(), null, cv);
                 messageAdapter.notifyDataSetChanged();
                 textBox.setText("");
             }
         });
+
+        while(!c.isAfterLast()) {
+            Log.i(activity, "SQL MESSAGE:"+c.getString(c.getColumnIndex(cDBH.KEY_MESSAGE)));
+        }
+
+        Log.i(activity, "Cursor’s column count =" + c.getColumnCount());
+
+        for (int x = 0; x<c.getColumnCount(); x++){
+            Log.i(activity, ""+c.getColumnName(x));
+        }
     }
 
     private class ChatAdapter extends ArrayAdapter<String>{
@@ -70,4 +102,11 @@ public class ChatWindow extends AppCompatActivity {
 
         }
     }
+
+    @Override
+    protected void onDestroy(){
+        dbWrite.close();
+        super.onDestroy();
+    }
+
 }
