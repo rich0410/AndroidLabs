a3f7295fd9425c838aca4ce93f116740069e9c0c
Justin <rich0410@algonquinlive.com>
2017-03-06 16:16:39 -0500

    Lab6 V2

--- app/src/main/java/com/example/justin/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/justin/androidlabs/WeatherForecast.java
@@ -4,8 +4,17 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Xml;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 public class WeatherForecast extends AppCompatActivity {
 
@@ -26,7 +35,33 @@ public class WeatherForecast extends AppCompatActivity {
         Bitmap wPic;
         @Override
         protected String doInBackground(String... params) {
-            return null;
+            String urlString = "http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=d99666875e0e51521f0040a3d97d0f6a&mode=xml&units=metric";
+            try {
+                URL url = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            InputStream in = conn.getInputStream();
+
+            try {
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, null);
+                parser.nextTag();
+                return String.valueOf(parser);
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } finally {
+                in.close();
+            }
+            }catch (java.io.IOException ioE){
+                ioE.getMessage();
+            }
         }
     }
 }
