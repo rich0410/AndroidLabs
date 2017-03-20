355153ecad3250aab78f46613f565b2ab8ce51d0
Justin <rich0410@algonquinlive.com>
2017-03-20 15:32:20 -0400

    Lab7 V1

---
+++ app/src/main/java/com/example/justin/androidlabs/MessageFragment.java
@@ -0,0 +1,20 @@
+package com.example.justin.androidlabs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Justin on 2017-03-20.
+ */
+
+public class MessageFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
